9d62aa7f24fbf22cc52138dd60701f0f25cf1777
Quigibrough <quigibrough@gmail.com>
2012-10-11 23:25:11 -0400

    Tank dirt interaction force into land

--- src/poomonkeys/common/Tank.java
+++ src/poomonkeys/common/Tank.java
@@ -5,54 +5,65 @@ import javax.media.opengl.GL2;
 public class Tank extends Drawable
 {
 	static final float TURRENT_LENGTH = 1f;
-	static final float WIDTH = 2f; 
-	static final float HEIGHT = 2f; 
+	static final float WIDTH = 4f;
+	static final float HEIGHT = 4f;
 
 	float baseGeometry[];
-	
+
 	public Drawable turret = new Drawable();
 	float turretLength = 1;
-	
+
 	public Tank()
 	{
 		width = WIDTH;
 		height = HEIGHT;
-		m=2;
+		m = 5;
 		this.registerDrawable(turret);
 	}
-	
+
 	public void buildGeometry(float viewWidth, float viewHeight)
 	{
 		float[] baseGeometry = {
-		            // X, Y
-					-width/2, -height/2, 0,
-					width/2, -height/2f,0,
-					0, height/2,0
-		        };
+				// X, Y
+				-width / 2, -height / 2, 0, width / 2, -height / 2f, 0, 0, height / 2, 0 };
 
 		this.drawMode = GL2.GL_LINE_LOOP;
 		super.vertices = baseGeometry;
-		
+
 		turretLength = TURRENT_LENGTH;
-		float turretGeometry[] = {
-				0, 0, 0,
-				0, turretLength, 0
-			};
+		float turretGeometry[] = { 0, 0, 0, 0, turretLength, 0 };
 		turret.drawMode = GL2.GL_LINES;
 		turret.vertices = turretGeometry;
 		turret.p[0] = 0;
-		turret.p[1] = height/2;
+		turret.p[1] = height / 2;
 	}
-	
+
 	public void intersectTerrain(Terrain t, float[] intersect)
 	{
 		removeFromPhysicsEngine = true;
-		this.p[0] += this.v[0]*intersect[2];
-		this.p[1] += this.v[1]*intersect[2];
+		this.p[0] += this.v[0] * intersect[2];
+		this.p[1] += this.v[1] * intersect[2];
+		float velocityVector = (float) Math.sqrt((this.v[0] * this.v[0]) + this.v[1] * this.v[1]);
+		float force = velocityVector * m;
+
+		if (force > 0)
+		{
+			this.p[0] = this.p[0] + this.v[0] * m;
+			this.p[1] = this.p[1] + this.v[1] * m;
+			t.explodeRectangle(this.p[0], this.p[1], width / 2);
+		}
+
 		this.v[0] = 0;
 		this.v[1] = 0;
+
+		// if (t.points[(int) (intersect[0] / t.segmentWidth)] > t.points[(int)
+		// (intersect[0] / t.segmentWidth) + 1])
+		// {
+		// this.p[0] = intersect[0] + t.segmentWidth;
+		// this.p[1] = t.points[(int) (intersect[0] / t.segmentWidth) + 1];
+		// }
 	}
-	
+
 	public void underTerrain()
 	{
 		this.v[0] = 0;
--- src/poomonkeys/common/Terrain.java
+++ src/poomonkeys/common/Terrain.java
@@ -35,6 +35,130 @@ public class Terrain extends Drawable
 		update();
 		tank.p[1] = average + tank.height/2;
 	}
+	//explodes width 2r from x,y and above
+	public void explodeRectangle(float x, float y, float r)
+	{
+		// The min and max terrain index that lies within the tank's radius
+		int min_index = Math.max(0, (int) ((x - r) / segmentWidth));
+		int max_index = Math.min(NUM_POINTS - 1, (int) ((x + r) / segmentWidth) + 1);
+
+		// The actual x value at the min and max indexes
+		float min_x = (min_index) * segmentWidth;
+		float max_x = (max_index) * segmentWidth;
+		
+		float totalDirtVolume = 0;
+		ArrayList<Dirt> dirt = null;
+		synchronized(this)
+		{
+			dirt = _generateDirtpointsRectangle(min_x, max_x, x, y, r);
+			totalDirtVolume = _collapseToRectangleBottom(min_index, max_index, x, y, r);
+		}
+		// Assign a portion of the removed dirt volume to each dirt point
+		float individualDirtVolume = totalDirtVolume / dirt.size();
+		for (int d = 0; d < dirt.size(); d++)
+		{
+			Dirt dirtPoint = dirt.get(d);
+			dirtPoint.volume = individualDirtVolume;
+		}
+		PhysicsController physicsController = PhysicsController.getInstance(this);
+		physicsController.addCollidables((ArrayList<? extends Drawable>)dirt);
+		float[] f = new float[3];
+		f[0] = x; f[1] = y; f[2] = 10;
+		physicsController.pointForces.add(f);
+	}
+	
+	
+	private ArrayList<Dirt> _generateDirtpointsRectangle(float min_x, float max_x, float x, float y, float r)
+	{
+		ArrayList<Dirt> dirt = new ArrayList<Dirt>();
+		
+		float explosion_width = Math.abs(max_x - min_x);
+		int num_dirt_columns = (int) ((explosion_width + segmentWidth) / (2 * DIRT_SIZE));
+		float gapTotal = (explosion_width + segmentWidth) % (2 * DIRT_SIZE);
+		float gap = gapTotal / num_dirt_columns;
+		float col_x = min_x - segmentWidth/2 + DIRT_SIZE + gap/2;
+		float end_x = max_x + segmentWidth/2 - DIRT_SIZE - gap/2;
+		float EPSILON = .0001f;
+		for (; col_x <= end_x + EPSILON; col_x += DIRT_SIZE * 2 + gap)
+		{
+			_generateDirtColumnRectangle(col_x, x, y, r, gap, dirt);
+		}
+
+		return dirt;
+	}
+	
+	private float _generateDirtColumnRectangle(float col_x, float x, float y, float r, float gap, ArrayList<Dirt> dirt)
+	{
+		float thing = (float) (r * r - Math.pow(col_x - x, 2));
+		float offset = 0;
+		if(thing > 0)
+		{
+			offset = (float) Math.sqrt(thing);
+		}
+		else
+		{
+			offset = 0;
+		}
+		float tCircleY = (float) (offset + y);
+
+		int iFromX = (int) (col_x / segmentWidth);
+		if(iFromX < 0 || iFromX >= points.length-1)
+		{
+			return 0;
+		}
+		
+		float xPercent = (col_x % segmentWidth) / segmentWidth;
+		float p1y = points[iFromX];
+		float p2y = points[iFromX + 1];
+		float top = p1y + (p2y - p1y) * xPercent;
+		if (top > tCircleY)
+		{
+			float d = 0;
+			while (tCircleY + d <= top)
+			{
+				Dirt dirtPoint = new Dirt();
+				dirtPoint.p[0] = col_x;
+				dirtPoint.p[1] = tCircleY + d;
+				dirtPoint.setWidth(DIRT_SIZE * 2 + gap);
+				dirtPoint.setHeight(DIRT_SIZE * 2 + gap);
+				dirt.add(dirtPoint);
+				registerDrawable(dirtPoint);
+				d += DIRT_SIZE * 2 + gap;
+			}
+		}
+
+		return top - tCircleY;
+	}
+	
+	private float _collapseToRectangleBottom(int min_index, int max_index, float x, float y, float r)
+	{
+		float dirtVolume = 0;
+
+		for (int i = min_index; i <= max_index; i++)
+		{
+			//change r for rectangles
+			float offset = (float) r;
+			float tCircleY = (float) (offset + y);
+			float bCircleY = (float) (y - offset);
+
+			// checks if the land is above the bottom of the circle, otherwise
+			// don't mess with it.
+			if(points[i] > bCircleY)
+			{
+				if(points[i] > tCircleY)
+				{
+					dirtVolume += points[i] - tCircleY;
+				}
+
+				points[i] = bCircleY;
+				previousPoints[i] = bCircleY;
+			}
+		}
+		buildGeometry(width, height);
+		finalizeGeometry();
+
+		return dirtVolume;
+	}
 	
 	// x and y are relative to the bottom left of the terrain
 	public void explodeCircle(float x, float y, float r)
